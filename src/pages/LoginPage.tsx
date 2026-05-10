020707f4cf3ae9010ef5377432e604586328b965
ryanjoel24 <ryanjoelimba@gmail.com>
2026-05-10 21:57:40 +0800

    final upload

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,7 +5,6 @@ import aquatec_bg from "@/assets/aquatech_bg2.jpg";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
-import { FaUserTie } from "react-icons/fa6";
 import {
   Dialog,
   DialogContent,
